c1dc5950f1ad25181cf3a1c1ff30b9d66eb5e990
rubidhexcy <dhexcyrubi@gmail.com>
2025-08-11 21:41:42 -0700

    add part3: Number stream

---
+++ lib/part3_stream.dart
@@ -0,0 +1,17 @@
+Stream<int> numberStream() async* {
+  for (var i = 1; i <= 5; i++) {
+    yield i;
+    await Future.delayed(Duration(seconds: 1));
+  }
+}
+
+void listenToNumbers() {
+  numberStream().listen((num) {
+    print('New: $num');
+  });
+}
+
+void main() {
+  listenToNumbers();
+  print('Listening to numbers...');
+}
