5a3d7012f2168ab1c988078e502db547ef21b366
Zelote-code <joezzelleramirez39@gmail.com>
2025-08-12 03:51:35 -0700

    Update part2_error_handling.dart

--- lib/part2_error_handling.dart
+++ lib/part2_error_handling.dart
@@ -1,19 +1,16 @@
 // File: part2_future_error_handling.dart
 Future<String> fetchData() {
-return Future.delayed(Duration(seconds: 2), () {
-throw Exception('Something went wrong!');
-});
-}
-void getSafeMessage() async {
-try {
-String message = await fetchData();
-print('Result: $message');
-} catch (e) {
-print('Caught an error: $e');
-}
+  return Future.delayed(Duration(seconds: 2), () {
+    throw Exception('Something went wrong!');
+  });
 }
 
-void main() {
-getSafeMessage();
-print('Still running even if something goes wrong.');
+void main() async {
+  try {
+    String message = await fetchData();
+    print('Result: $message');
+  } catch (e) {
+    print('Caught an error: $e');
+  }
+  print('Still running even if something goes wrong.');
 }
